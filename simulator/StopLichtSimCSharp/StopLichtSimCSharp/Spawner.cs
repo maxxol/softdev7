791d93c2f6593f0729dc2cf0c315217dd8541f44
Jeroen Lüling <jtube2014@gmail.com>
2025-04-22 18:56:21 +0200

    boat class

--- simulator/StopLichtSimCSharp/StopLichtSimCSharp/Spawner.cs
+++ simulator/StopLichtSimCSharp/StopLichtSimCSharp/Spawner.cs
@@ -8,7 +8,7 @@ namespace StopLichtSimCSharp
 {
     class Spawner
     {
-        int numberOfCarLanes = 0; //update when nodes have been set.
+        int numberOfCarLanes = 0; //update manually when nodes have been set.
         int numberOfBikeLanes = 0;
         int numberOfPedLanes = 0;
         int numberOfBoatLanes = 2;
@@ -26,7 +26,7 @@ namespace StopLichtSimCSharp
             if (chosenLaneNumber <= numberOfCarLanes+1) { spawnCar(chosenLane); } //car
             else if (chosenLaneNumber <= numberOfBikeLanes + numberOfCarLanes) { spawnBike(chosenLane); } //bike
             else if (chosenLaneNumber <= numberOfPedLanes + numberOfCarLanes + numberOfBikeLanes) { spawnPed(chosenLane); } //ped
-            else if (chosenLaneNumber <= numberOfBoatLanes + numberOfCarLanes + numberOfBikeLanes + numberOfPedLanes) { } //boat
+            else if (chosenLaneNumber <= numberOfBoatLanes + numberOfCarLanes + numberOfBikeLanes + numberOfPedLanes) { spawnBoat(chosenLane); } //boat
             else { }//number outside of array count (should be impossible)
 
         }
