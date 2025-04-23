fe3bdb5e7cf641ef687d65aad79b537d0446fa25
Jeroen Lüling <jtube2014@gmail.com>
2025-04-23 10:28:44 +0200

    drawing spawned cars

--- simulator/StopLichtSimCSharp/StopLichtSimCSharp/Spawner.cs
+++ simulator/StopLichtSimCSharp/StopLichtSimCSharp/Spawner.cs
@@ -8,43 +8,53 @@ namespace StopLichtSimCSharp
 {
     class Spawner
     {
-        int numberOfCarLanes = 0; //update manually when nodes have been set.
+        int numberOfCarLanes = 3; //update manually when nodes have been set.
         int numberOfBikeLanes = 0;
         int numberOfPedLanes = 0;
-        int numberOfBoatLanes = 2;
+        int numberOfBoatLanes = 0;
 
 
 
-        List<RoadUser> allRoadUsersArray;
+        static List<RoadUser> allRoadUsersList = new List<RoadUser>();
 
 
-        public void spawnRoaduser(Lane[] Lanes)
+        public RoadUser[] spawnRoaduser(Lane[] Lanes)
         {
             var rand = new Random();
-            int chosenLaneNumber = rand.Next(allRoadUsersArray.Count);
+            int chosenLaneNumber = rand.Next(Lanes.Length);
             Lane chosenLane = Lanes[chosenLaneNumber]; //choose random lane to spawn a car
-            if (chosenLaneNumber <= numberOfCarLanes+1) { spawnCar(chosenLane); } //car
-            else if (chosenLaneNumber <= numberOfBikeLanes + numberOfCarLanes) { spawnBike(chosenLane); } //bike
-            else if (chosenLaneNumber <= numberOfPedLanes + numberOfCarLanes + numberOfBikeLanes) { spawnPed(chosenLane); } //ped
-            else if (chosenLaneNumber <= numberOfBoatLanes + numberOfCarLanes + numberOfBikeLanes + numberOfPedLanes) { spawnBoat(chosenLane); } //boat
+            if (rand.Next(300) == 1) { 
+            if (chosenLaneNumber <= numberOfCarLanes) { spawnCar(chosenLane, chosenLaneNumber); } //car
+            else if (chosenLaneNumber <= numberOfBikeLanes + numberOfCarLanes) { spawnBike(chosenLane, chosenLaneNumber); } //bike
+            else if (chosenLaneNumber <= numberOfPedLanes + numberOfCarLanes + numberOfBikeLanes) { spawnPed(chosenLane, chosenLaneNumber); } //ped
+            else if (chosenLaneNumber <= numberOfBoatLanes + numberOfCarLanes + numberOfBikeLanes + numberOfPedLanes) { spawnBoat(chosenLane, chosenLaneNumber); } //boat
             else { }//number outside of array count (should be impossible)
+                    }
+            return allRoadUsersList.ToArray();
 
         }
-        public void spawnCar(Lane chosenLane)
+        public void spawnCar(Lane chosenLane, int chosenLaneNumber)
         {
-            allRoadUsersArray.Add(new Car(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y));
+            Console.WriteLine("spawned car");
+            allRoadUsersList.Add(new Car(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y, chosenLaneNumber));
         }
-        public void spawnBike(Lane chosenLane)
+        public void spawnBike(Lane chosenLane, int chosenLaneNumber)
         {
-            allRoadUsersArray.Add(new Bike(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y));
+            Console.WriteLine("spawned bike");
+
+            allRoadUsersList.Add(new Bike(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y, chosenLaneNumber));
         }
-        public void spawnPed(Lane chosenLane)
+        public void spawnPed(Lane chosenLane, int chosenLaneNumber)
         {
-            allRoadUsersArray.Add(new Pedestrian(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y));
+            Console.WriteLine("spawned ped");
+
+            allRoadUsersList.Add(new Pedestrian(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y, chosenLaneNumber));
         }
-        public void spawnBoat(Lane chosenLane)
+        public void spawnBoat(Lane chosenLane, int chosenLaneNumber)
         {
-            allRoadUsersArray.Add(new Boat(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y));
+            Console.WriteLine("spawned boat");
+
+            allRoadUsersList.Add(new Boat(chosenLane.CheckPointNodes[0].X, chosenLane.CheckPointNodes[0].Y, chosenLaneNumber));
         }
     }
 }
