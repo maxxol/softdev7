f08b050029dc84d3da1d795f0aa9921785d53af5
AllardArcade <allard.de.groot@student.nhlstenden.com>
2025-06-25 10:18:48 +0200

    nodes corrected

--- simulator/StopLichtSimCSharp/StopLichtSimCSharp/SensorenSpeciaal.cs
+++ simulator/StopLichtSimCSharp/StopLichtSimCSharp/SensorenSpeciaal.cs
@@ -28,41 +28,41 @@ namespace StopLichtSimCSharp
         public static string bridgeSensors(Lane[] lanes)
         {
             var result = new Dictionary<string, Dictionary<string, bool>>();
-            //foreach (Lane lane in lanes)
-            //{
-            //    foreach (CheckPointNode node in lane.CheckPointNodes)
-            //    {
-            //        try
-            //        {
-            //            if (brugNodes.TryGetValue(node.NodeID, out string sensorID))
-            //            {
-                            
-            //                if (parts.Length == 3)
-            //                {
-            //                    string groupID = $"{parts[0]}"; // e.g., "1.1"
-            //                   // string position = parts[1]; // "voor" or "achter"
+            foreach (Lane lane in lanes)
+            {
+                foreach (CheckPointNode node in lane.CheckPointNodes)
+                {
+                    try
+                    {
+                        if (brugNodes.TryGetValue(node.NodeID, out string sensorID))
+                        {
 
-            //                    if (!result.ContainsKey(groupID))
-            //                    {
-            //                        result[groupID] = new Dictionary<string, bool>
-            //                        {
-            //                            { "brug_wegdek", false },
-            //                            { "brug_water", false },
-            //                            { "brug_file", false }
-            //                        };
-            //                    }
-            //                    result[groupID][position] = node.Occupied;
-            //                }
-            //            }
-            //        }
-            //        catch
-            //        {
+                           // if (parts.Length == 3)
+                            //{
+                                string groupID = sensorID; // e.g., "1.1"
+                                                                // string position = parts[1]; // "voor" or "achter"
 
-            //        }
-            //    }
-            //}
+                                if (!result.ContainsKey(groupID))
+                                {
+                                    result[groupID] = new Dictionary<string, bool>
+                                    {
+                                        { "brug_wegdek", false },
+                                        { "brug_water", false },
+                                        { "brug_file", false }
+                                    };
+                                }
+                               // result[groupID] = node.Occupied;
+                            //}
+                        }
+                    }
+                    catch
+                    {
 
-            string newjs = $"{{\n \"brug_wegdek\":  false\n  \"brug_water\": false\n  \"brug_file\": false\n}}";
+                    }
+                }
+            }
+
+            string newjs = $"{{\n \"brug_wegdek\":  false,\n  \"brug_water\": false,\n  \"brug_file\": true\n}}";
             string json = JsonConvert.SerializeObject(result, Formatting.Indented);
             Console.WriteLine(newjs);
             return newjs;
